b95c6fbfd7d56fef5e3789c1225f4f168a52f7ea
Jared Orler <orlerdev@icloud.com>
2023-05-16 15:40:54 -0500

    initial commit

---
+++ src/main/java/com/codeup/codeupadlister/CodeupAdlisterApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.codeupadlister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeupAdlisterApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CodeupAdlisterApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/codeup/codeupadlister/CodeupAdlisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.codeupadlister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeupAdlisterApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
